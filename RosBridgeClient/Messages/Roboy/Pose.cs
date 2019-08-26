e307b5bb53f3b099277ab8495d16e85208d716fb
Marla Narazani <marla.narazani@tum.de>
2019-08-26 19:39:52 +0200

    added pose message type

---
+++ RosBridgeClient/Messages/Roboy/Pose.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+using RosSharp.RosBridgeClient.Messages.Geometry;
+
+namespace RosSharp.RosBridgeClient.Messages.Roboy
+{
+    public class Pose : Message
+    {
+        [JsonIgnore]
+        public const string RosMessageName = "roboy_middleware_msgs/Pose";
+
+        public UInt16 id;
+        public Point position;
+        public Quaternion orientation;
+
+        public Pose()
+        {
+            id = new int();
+            position = new Point();
+            orientation = new Quaternion();
+        }
+    }
+}
