ac96f9243a9bb307ac7598fd849fafd7472f1c25
Leenert Specht <leenert.specht@gmail.com>
2019-08-28 20:59:07 +0200

    added interfaces

---
+++ RosBridgeClient/Messages/Roboy/FeedbackDemo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace RosSharp.RosBridgeClient.Messages.Roboy
+{
+    public class FeedbackDemo : Message
+    {
+        [JsonIgnore]
+        public const string RosMessageName = "roboy_middleware_msgs/FeedbackDemo";
+
+        public Int32 status;
+
+        public FeedbackDemo()
+        {
+            status = new Int32();
+        }
+    }
+}
---
+++ RosBridgeClient/Messages/Roboy/InterfaceStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace RosSharp.RosBridgeClient.Messages.Roboy
+{
+    public class InterfaceStatus : Message
+    {
+        [JsonIgnore]
+        public const string RosMessageName = "roboy_middleware_msgs/InterfaceStatus";
+
+        public Int32 status;
+
+        public InterfaceStatus()
+        {
+            status = new Int32();
+        }
+    }
+}
