d8019a300e20c2a87c78b0e3cdd1e38c5ef7b1c5
Leenert Specht <leenert.specht@gmail.com>
2019-08-30 16:30:52 +0200

    added test service

---
+++ RosBridgeClient/Services/Roboy/VRPuppetStateTransmission.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+namespace RosSharp.RosBridgeClient.Services.Roboy
+{
+    public class VRPuppetStateTransmissionRequest : Message
+    {
+        [JsonIgnore]
+        public const string RosMessageName = "vr_puppets/state_transmission";
+
+        public bool data;
+
+        public VRPuppetStateTransmissionRequest(bool data)
+        {
+            this.data = data;
+        }
+    }
+
+    public class VRPuppetStateTransmissionResponse : Message
+    {
+        [JsonIgnore]
+        public const string RosMessageName = "vr_puppets/state_transmission";
+        public bool success;
+        public string message;
+        public VRPuppetStateTransmissionResponse(bool success, string message)
+        {
+            this.success = success;
+            this.message = message;
+        }
+    }
+}
